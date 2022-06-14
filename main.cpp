4cf76b4e38b707f13d4038257c3b30d363a7c53b
OoShawnoO <840831038@qq.com>
2022-06-14 11:22:38 +0800

    完成

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <fstream>
+
 #include "main.h"
+
+using namespace std;
 
 template <class type>
 void LinearList<type>::EmptyList() {
@@ -45,7 +49,7 @@ int LinearList<type>::Index(type x) {
     for(;index<Len;index++){
         if(x==this->list[index]){return index;}
     }
-    return NULL;
+    return -1;
 }
 
 template <class type>
@@ -99,20 +103,15 @@ int LinearList<type>::IsEmpty() {
     }
 }
 
-template <class type>
-void LinearList<type>::Initialize() {
-    this->length = 0;
-    EmptyList();
-}
 
 template <class type>
 void LinearList<type>::Traverse() {
     for(int i=0;i<this->length;i++){
-        printf("%d ",i);
+        cout<<i<<' ';
     }
     printf("\n");
     for(int i=0;i<this->length;i++){
-        printf("%d ",this->list[i]);
+        cout<<list[i]<<' ';
     }
     printf("\n");
 }
@@ -144,67 +143,313 @@ int LinearList<type>::Sort() {
 
 /* Link List Define*/
 
-template <class type>
-int LinkList<type>::Length(){
-    int count = 0;
-    while(next!=NULL){
-        count++;
-        this = this->next;
-    }
-    return count;
+template<class type>
+LinkList<type>::LinkList(const LinkList & obj){
+    head = obj.head;
+    length = obj.length;
 }
 
-template <class type>
-void LinkList<type>::Initialize() {
-    auto ptr = new LinkList<type>;
-    ptr.data=-1;
-    ptr->next = nullptr;
-    this = ptr;
+template<class type>
+LinkList<type>::~LinkList(){
+//    auto ptr = head;
+//    while(ptr){
+//        auto temp = ptr->next;
+//        delete ptr;
+//        ptr = temp;
+//    }
 }
 
-template <class type>
-int LinkList<type>::Insert(type x,int index){
-    if(index>Length()){return -1;}
-    else{
-        auto ptr = this;
+template<class type>
+int LinkList<type>::GetLength() {
+    return length;
+}
 
-        int i = 0;
-        while(i<index-1){
+template<class type>
+type LinkList<type>::Get(int index) {
+    if(index<=length&&index>=0){
+        auto ptr = head;
+        while(index>0){
             ptr = ptr->next;
-            i++;
+            index--;
         }
-        auto aptr = ptr->next;
-        if(aptr!=nullptr){
-            auto y = new LinkList<type>;
-            y->data = x;
-            y->next = aptr;
-            ptr->next = y;
+        return ptr->data;
+    }
+    else{
+        return (type)NULL;
+    }
+}
+
+template<class type>
+type LinkList<type>::Pre(int index) {
+    return Get(index-1);
+}
+
+template<class type>
+type LinkList<type>::Next(int index) {
+    return Get(index+1);
+}
+
+template<class type>
+int LinkList<type>::Index(type x) {
+    auto ptr = head;
+    int count = 0;
+    while(ptr->data!=x && ptr != nullptr){
+        ptr = ptr->next;
+        count++;
+    }
+    if(ptr!=nullptr)
+        return count;
+    else
+        return -1;
+}
+
+template<class type>
+int LinkList<type>::Insert(type x, int index) {
+    auto node = new LinkNode;
+    node->data = x;
+    node->next = nullptr;
+    length++;
+    if(index>length){
+        auto ptr = head;
+        while(ptr->next!=nullptr){
+            ptr = ptr->next;
+        }
+        ptr->next = node;
+        return 1;
+    }
+    else if(index==0){
+        node->next = head->next;
+        head->next = node;
+        return 1;
+    }
+    else{
+        auto ptr = head;
+        while(index>0){
+            ptr = ptr->next;
+            index--;
+        }
+        auto temp = ptr->next;
+        ptr->next = node;
+        node->next = temp;
+        return 1;
+    }
+}
+
+template<class type>
+void LinkList<type>::Add(type x){
+    Insert(x,9999);
+}
+
+template<class type>
+int LinkList<type>::DeleteByVar(type x) {
+    auto ptr = head;
+    while(ptr){
+        if(ptr->next->data==x){
+            auto temp = ptr->next;
+            ptr->next = temp->next;
+            delete(temp);
             return 1;
         }
-        else{
-            auto y = new LinkList<type>;
-            y->data = x;
-            ptr->next = y;
-            return 1;
+        ptr = ptr->next;
+    }
+    return 0;
+}
+
+template<class type>
+int LinkList<type>::DeleteByIndex(int index) {
+    if(index>length){
+        return 0;
+    }
+    else{
+        auto ptr = head;
+        while(index>1){
+            ptr = ptr->next;
         }
+        auto temp = ptr->next;
+        ptr->next = temp->next;
+        delete(temp);
+        return 1;
+    }
+}
+
+template<class type>
+void LinkList<type>::Clear() {
+    delete(this);
+}
+
+template<class type>
+int LinkList<type>::IsEmpty() {
+    if(length==0){return 1;}
+    else{return 0;}
+}
+
+template<class type>
+void LinkList<type>::Traverse() {
+    auto ptr = head->next;
+    while(ptr){
+        cout<<(*ptr).data<<' ';
+        ptr = ptr->next;
+    }
+}
+
+template<class type>
+int LinkList<type>::Update(int index, type x) {
+    if(index>length){return 0;}
+    auto ptr = head;
+    while(index>0){
+        ptr = ptr->next;
+    }
+    ptr->data = x;
+    return 1;
+}
+
+template<class type>
+void Stack<type>::Push(type x){
+    LinearList<type>::Insert(x,LinearList<type>::length);
+}
+
+template<class type>
+void Stack<type>::Pop(type* x){
+    *x = LinearList<type>::Get(LinearList<type>::length-1);
+    LinearList<type>::DeleteByIndex(LinearList<type>::length-1);
+}
+
+template<class type>
+int Stack<type>::GetTop(){return LinearList<type>::length;}
+
+template<class type>
+void Queue<type>::InQueue(type x){
+    LinearList<type>::Insert(x,LinearList<type>::length);
+}
+
+template<class type>
+void Queue<type>::OutQueue(type* x){
+    *x = LinearList<type>::Get(0);
+    LinearList<type>::DeleteByIndex(0);
+}
+
+template<class type>
+void LinkList<type>::operator+(const LinkList<type> & linkList){
+    auto ptr = linkList.head->next;
+    while(ptr){
+        Add(ptr->data);
+        ptr = ptr->next;
     }
 }
 
 
 int main() {
-    LinearList<int> ls;
-    for(int j=0;j<20;j++){
-        ls.Insert(rand()%100,j);
-    }
-    ls.Traverse();
+    /*---????---*/
+//    LinearList<int> ls;
+//    for(int j=0;j<20;j++){
+//        ls.Insert(rand()%100,j);
+//    }
+//    ls.Traverse();
 //    printf("%d %d",ls.Length(),ls.Get(2));
-    ls.Sort();
-    ls.Traverse();
-    LinearList<int> l2 = ls;
-    l2.Update(0,100);
-    l2.Traverse();
-    ls.Traverse();
-    ls.Clear();
-    ls.Traverse();
+//    ls.Sort();
+//    ls.Traverse();
+//    LinearList<int> l2 = ls;
+//    l2.Update(0,100);
+//    l2.Traverse();
+//    ls.Traverse();
+//    ls.Clear();
+//    ls.Traverse();
+    /*---????---*/
+//    LinkList<int> ls;
+//    ls.Insert(2,0);
+//    LinkList<int> l2 = ls;
+//
+//    ls.Add(4);
+//    ls.Traverse();
+//    cout << ls.GetLength()<<'\n';
+//    cout << l2.GetLength()<<'\n';
+    /*---?---*/
+    /*
+     * 1
+Out of List!0
+     * */
+//    Stack<int> stack;
+//    stack.Push(1);
+//    int x;
+//    stack.Pop(&x);
+//    cout<<x<<endl;
+//    stack.Pop(&x);
+//    cout<<x<<endl;
+    /*---????---*/
+    /*  1
+        2   */
+//    Queue<int> queue;
+//    queue.InQueue(1);
+//    queue.InQueue(2);
+//    int x;
+//    queue.OutQueue(&x);
+//    cout<<x<<endl;
+//    queue.OutQueue(&x);
+//    cout<<x<<endl;
+
+    /*---???---*/
+    /*  1
+        2
+
+        2
+        1
+    */
+//    LinearList<int>* ls;
+//    Queue<int> queue;
+//    Stack<int> stack;
+//    ls = &queue;
+//    int x=0;
+//    ls->In(1);
+//    ls->In(2);
+//    ls->Out(&x);
+//    cout<<x<<endl;
+//    ls->Out(&x);
+//    cout<<x<<endl;
+//
+//    cout<<""<<endl;
+//
+//    ls = &stack;
+//    ls->In(1);
+//    ls->In(2);
+//    ls->Out(&x);
+//    cout<<x<<endl;
+//    ls->Out(&x);
+//    cout<<x<<endl;
+
+    /*---?????????---*/
+    /*0 1 2 3 4 10 9 8 7*/
+//    LinkList<int> linkList1;
+//    LinkList<int> linkList2;
+//    for(int i=0;i<5;i++){linkList1.Add(i);}
+//    for(int i=10;i>6;i--){linkList2.Add(i);}
+//    linkList1+linkList2;
+//    linkList1.Traverse();
+
+    /*---??---*/
+    /*2*/
+//    try{
+//        throw 2;
+//    }catch(int x){
+//        printf("%d",x);
+//    }
+
+    /*---?????????---*/
+    ofstream file1("D://C_Learn/List/123.txt");
+    ifstream file("D://C_Learn/List/123.txt");
+
+    for(int i=0;i<100;i++){
+        cout<<i<<endl;
+        file1<< 'a';
+    }
+    file1.close();
+    char x=' ';
+    while(file.get(x)){
+        cout<<x<<endl;
+    }
+    file.close();
 
 }
+
+
+
+
--- main.h
+++ main.h
@@ -10,18 +10,20 @@ template<class type>
 class LinearList{
 private:
     type list[Len];
-    int length;
+
 public:
-    LinearList():length(0)
-    {
-        Initialize();
+    int length;
+    LinearList():length(0){
+        for(auto & i : list){
+            i = -1;
+        }
     }
     LinearList(const LinearList & l):length(l.length){
         for(int i=0;i<l.length;i++){
             list[i] = l.list[i];
         }
 //        printf("%p %p",l.list[0],list[0]);
-        this->length = l.length;
+        length = l.length;
     }
     void EmptyList();
     int Length();
@@ -34,44 +36,81 @@ public:
     int DeleteByIndex(int index);
     void Clear();
     int IsEmpty();
-    void Initialize();
     void Traverse();
     int Update(int index,type x);
     int Sort();
+    virtual void In(type x){};
+    virtual void Out(type* x){};
 };
+
+
+
 
 template<class type>
 class LinkList{
 private:
-    type data;
-    LinkList* next;
-public:
-    LinkList()
-    {
-        Initialize();
-    }
-    LinkList(const LinkList & obj){
-        auto *ptr = new LinkList<type>;
-        while(obj->next){
-            ptr->data = obj.data;
-            ptr->next = new LinkList;
-            ptr = ptr->next;
-            obj = obj->next;
+    class LinkNode{
+    public:
+        type data;
+        LinkNode* next;
+        LinkNode():next(nullptr),data(0){}
+        LinkNode(const LinkNode & obj){
+            data = *obj.data;
+            next = new LinkNode;
+            next = *obj.next;
         }
+    };
+    LinkNode* head;
+    int length;
+public:
+    LinkList(){
+        head = new LinkNode;
+        length = 0;
     }
-    int Length();
+    LinkList(const LinkList & obj);
+    ~LinkList();
+    int GetLength();
     type Get(int index);
     type Pre(int index);
     type Next(int index);
     int Index(type x);
     int Insert(type x,int index);
+    void Add(type x);
     int DeleteByVar(type x);
     int DeleteByIndex(int index);
     void Clear();
     int IsEmpty();
-    void Initialize();
     void Traverse();
     int Update(int index,type x);
-    int Sort();
+    void operator+(const LinkList<type> & linkList);
 };
+
+template<class type>
+class Stack:public LinearList<type>
+{
+public:
+    Stack()= default;
+    Stack(const Stack<type> & obj): LinearList<type>(obj){
+    }
+    void Push(type x);
+    void Pop(type* x);
+    void In(type x){Push(x);};
+    void Out(type* x){Pop(x);};
+    int GetTop();
+};
+
+template<class type>
+class Queue:public LinearList<type>
+{
+public:
+    Queue()=default;
+    Queue(const Queue<type> & obj): LinearList<type>(obj){}
+    void InQueue(type x);
+    void OutQueue(type *x);
+    void In(type x){ InQueue(x);};
+    void Out(type* x){ OutQueue(x);};
+
+};
+
+
 #endif //LIST_MAIN_H
